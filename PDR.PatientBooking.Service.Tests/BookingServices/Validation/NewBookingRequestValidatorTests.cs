59b99f1d0c0ebec245325f01ac69ab7463ac7aed
aybars <support@srabya.com>
2020-09-28 10:32:55 +0100

    T2 - Start validations - StartTime I dont have a polished plan yet, I'll see how it goes

--- PDR.PatientBooking.Service.Tests/BookingServices/Validation/NewBookingRequestValidatorTests.cs
+++ PDR.PatientBooking.Service.Tests/BookingServices/Validation/NewBookingRequestValidatorTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using AutoFixture;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Data.Models;
 using PDR.PatientBooking.Service.BookingServices.Requests;
 using PDR.PatientBooking.Service.BookingServices.Validation;
 
@@ -13,6 +16,7 @@ namespace PDR.PatientBooking.Service.Tests.BookingServices.Validation
         private IFixture _fixture;
 
         private NewBookingRequestValidator _newBookingRequestValidator;
+        private PatientBookingContext _context;
 
         [SetUp]
         public void SetUp()
@@ -23,15 +27,18 @@ namespace PDR.PatientBooking.Service.Tests.BookingServices.Validation
             //Prevent fixture from generating from entity circular references 
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior(1));
 
+            // Context setup
+            _context = new PatientBookingContext(new DbContextOptionsBuilder<PatientBookingContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+
             // Sut instantiation
-            _newBookingRequestValidator = new NewBookingRequestValidator();
+            _newBookingRequestValidator = new NewBookingRequestValidator(_context);
         }
 
         [Test]
         public void ValidateRequest_BookingInThePast_ReturnsFailedValidationResult()
         {
             //arrange
-            var request = _fixture.Create<NewBookingRequest>();
+            var request = GetValidRequest();
             request.StartTime = DateTime.Now.Subtract(TimeSpan.FromDays(1));
 
             //act
@@ -46,7 +53,7 @@ namespace PDR.PatientBooking.Service.Tests.BookingServices.Validation
         public void ValidateRequest_BookingInTheFuture_PassesValidation()
         {
             //arrange
-            var request = _fixture.Create<NewBookingRequest>();
+            var request = GetValidRequest();
             request.StartTime = DateTime.Now.AddDays(1);
 
             //act
@@ -54,6 +61,38 @@ namespace PDR.PatientBooking.Service.Tests.BookingServices.Validation
 
             //assert
             res.PassedValidation.Should().BeTrue();
+        }
+        [Test]
+        public void ValidateRequest_DoctorAlreadyBookedAtTheStartTime_ReturnsFailedValidationResult()
+        {
+            //arrange
+            //book a doctor
+            var existingBooking = _fixture
+                .Build<Order>()
+                .With(x => x.StartTime, DateTime.Now.AddMinutes(15))
+                .With(x => x.EndTime, DateTime.Now.AddMinutes(30))
+                .Create();
+
+            _context.Add(existingBooking);
+            _context.SaveChanges();
+            //setup a booking with the same doctor at the same time
+            var request = GetValidRequest();
+            request.StartTime = existingBooking.StartTime;
+            request.DoctorId = existingBooking.DoctorId;
+
+            //act
+            var res = _newBookingRequestValidator.ValidateRequest(request);
+
+            //assert
+            res.PassedValidation.Should().BeFalse();
+            res.Errors.Should().Contain("Doctor is already booked for the requested time.");
+        }
+
+
+
+        private NewBookingRequest GetValidRequest()
+        {
+            return _fixture.Create<NewBookingRequest>();
         }
 
     }
--- PDR.PatientBooking.Service/BookingServices/Validation/NewBookingRequestValidator.cs
+++ PDR.PatientBooking.Service/BookingServices/Validation/NewBookingRequestValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using PDR.PatientBooking.Data;
 using PDR.PatientBooking.Service.BookingServices.Requests;
 using PDR.PatientBooking.Service.Validation;
 
@@ -8,12 +9,20 @@ namespace PDR.PatientBooking.Service.BookingServices.Validation
 {
     public class NewBookingRequestValidator
     {
+        private readonly PatientBookingContext _context;
 
+        public NewBookingRequestValidator(PatientBookingContext context)
+        {
+            _context = context;
+        }
         public PdrValidationResult ValidateRequest(NewBookingRequest request)
         {
             var result = new PdrValidationResult(true);
 
             if (BookingInThePast(request, ref result))
+                return result;
+
+            if (DoctorAlreadyBooked(request, ref result))
                 return result;
 
             return result;
@@ -27,6 +36,25 @@ namespace PDR.PatientBooking.Service.BookingServices.Validation
 
             if (request.StartTime < DateTime.Now)
                 errors.Add("Bookings can not be in the past");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DoctorAlreadyBooked(NewBookingRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+
+            if (_context.Order.Any(x => x.DoctorId == request.DoctorId && x.StartTime == request.StartTime))
+                errors.Add("Doctor is already booked for the requested time.");
+
 
             if (errors.Any())
             {

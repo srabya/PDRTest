77729490557560aec508cddc5cfee528297ed0d9
aybars <support@srabya.com>
2020-09-28 10:28:45 +0100

    T2 - Adding validation for bookings in the past

---
+++ PDR.PatientBooking.Service.Tests/BookingServices/Validation/NewBookingRequestValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using PDR.PatientBooking.Service.BookingServices.Requests;
+using PDR.PatientBooking.Service.BookingServices.Validation;
+
+namespace PDR.PatientBooking.Service.Tests.BookingServices.Validation
+{
+    [TestFixture]
+    public class NewBookingRequestValidatorTests
+    {
+        private IFixture _fixture;
+
+        private NewBookingRequestValidator _newBookingRequestValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Boilerplate
+            _fixture = new Fixture();
+
+            //Prevent fixture from generating from entity circular references 
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior(1));
+
+            // Sut instantiation
+            _newBookingRequestValidator = new NewBookingRequestValidator();
+        }
+
+        [Test]
+        public void ValidateRequest_BookingInThePast_ReturnsFailedValidationResult()
+        {
+            //arrange
+            var request = _fixture.Create<NewBookingRequest>();
+            request.StartTime = DateTime.Now.Subtract(TimeSpan.FromDays(1));
+
+            //act
+            var res = _newBookingRequestValidator.ValidateRequest(request);
+
+            //assert
+            res.PassedValidation.Should().BeFalse();
+            res.Errors.Should().Contain("Bookings can not be in the past");
+        }
+
+        [Test]
+        public void ValidateRequest_BookingInTheFuture_PassesValidation()
+        {
+            //arrange
+            var request = _fixture.Create<NewBookingRequest>();
+            request.StartTime = DateTime.Now.AddDays(1);
+
+            //act
+            var res = _newBookingRequestValidator.ValidateRequest(request);
+
+            //assert
+            res.PassedValidation.Should().BeTrue();
+        }
+
+    }
+}
---
+++ PDR.PatientBooking.Service/BookingServices/Validation/NewBookingRequestValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PDR.PatientBooking.Service.BookingServices.Requests;
+using PDR.PatientBooking.Service.Validation;
+
+namespace PDR.PatientBooking.Service.BookingServices.Validation
+{
+    public class NewBookingRequestValidator
+    {
+
+        public PdrValidationResult ValidateRequest(NewBookingRequest request)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (BookingInThePast(request, ref result))
+                return result;
+
+            return result;
+        }
+
+        private bool BookingInThePast(NewBookingRequest request, ref PdrValidationResult result)
+        {
+
+
+            var errors = new List<string>();
+
+            if (request.StartTime < DateTime.Now)
+                errors.Add("Bookings can not be in the past");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
